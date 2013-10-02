50d208f896708d5aee71665000eef5b38e24f99c
FlowerPot <flowerpot@pondati.net>
2013-10-02 22:32:48 +0200

    Initial commit, added basis of rails.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+VimPastebin::Application.config.secret_key_base = '860b7d43516a125b387a75fb4e5137ecc86a094b03da238755b5d40d46ac01546b42ed86289615dcc04054aa2483508ca06c2ad78824dd11c71bbf88bbc38e8f'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+VimPastebin::Application.config.session_store :cookie_store, key: '_vim-pastebin_session'
