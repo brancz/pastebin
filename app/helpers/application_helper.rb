3b1976da9131175c7693b3ee817d014c1cfa4dc2
flower-pot <flowerpot@pondati.net>
2013-10-11 22:56:32 +0200

    Added ability of nested layouts.

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,7 @@
 module ApplicationHelper
+  def inside_layout(layout, &block)
+    layout = "layouts/#{layout}" unless layout =~ %r[\Alayouts/]
+    content_for :content, capture(&block)
+    render template: layout
+  end
 end
