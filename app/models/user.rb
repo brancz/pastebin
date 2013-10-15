f965b5e56a9ac5cad8dcd638c38dce8bc42f1e40
flower-pot <flowerpot@pondati.net>
2013-10-15 12:08:48 +0200

    Added complete GitHub OAuth support

--- app/models/user.rb
+++ app/models/user.rb
@@ -10,19 +10,24 @@ class User < ActiveRecord::Base
     :case_sensitive => false
   }
 
-  has_many :pastes
+    has_many :pastes
 
-  def self.find_for_github_oauth2(access_token, signed_in_resource=nil)
-    data = access_token.info
-    user = User.where(:email => data["email"]).first
+    def self.find_for_github_oauth2(access_token, signed_in_resource=nil)
+      data = access_token.info
+      puts "#########"
+      puts access_token
+      puts "#########"
+      user = User.where(:email => data["email"]).first
 
-    unless user
-      user = User.create(name: data["name"],
-                          email: data["email"],
-                          password: Devise.friendly_token[0,20]
-                        )
+      unless user
+        user = User.create(name: data["nickname"],
+                           email: data["email"],
+                           provider: access_token.provider,
+                           uid: access_token.uid,
+                           password: Devise.friendly_token[0,20]
+                          )
+      end
+      user
     end
-    user
-  end
 
 end
